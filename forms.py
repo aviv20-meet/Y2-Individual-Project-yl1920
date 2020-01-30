0ecd6dc5c5f8a660a104bc336fde62be96db5884
myname21-meet <myname21@meet.mit.edu>
2020-01-30 17:19:17 +0200

    panic

--- forms.py
+++ forms.py
@@ -12,7 +12,3 @@ class Register_form(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
-class Make_post(FlaskForm):
-	post_name = StringField('post name', validators = [InputRequired(), Length(min=1,max=64)])
-	post_text = StringField('post contents' , validators = [Length(min = 0,max = 3000)])
-	time_post = IntegerField('post time', validators = [InputRequired()])
